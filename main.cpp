811333bb07577bdeaf840c11b5d0d6bb633d3db7
Karim Shebl <101910065+KarimShebl@users.noreply.github.com>
2023-12-25 07:40:53 +0200

    Interval Tree Construction

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
+struct Interval {
+    int left, right, maxRight;
+    Interval *leftSubTree, *rightSubTree, *parent;
+
+    Interval() : leftSubTree(nullptr), rightSubTree(nullptr), parent(nullptr) {}
+
+    Interval(int left, int right, int maxRight) :
+            left(left), right(right), maxRight(maxRight), leftSubTree(nullptr), rightSubTree(nullptr),
+            parent(nullptr) {}
+};
+
+class IntervalTree {
+    Interval *root{};
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    IntervalTree intervalTree;
     return 0;
 }
